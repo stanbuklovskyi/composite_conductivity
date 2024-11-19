8ba67a1eed49b12f410ffe03133ff524f4789c7e
Stanislav Buklovskyi <sb1454@usnh.edu>
2024-11-19 16:59:24 -0500

    Adds correct dat files setup with names check

--- dat_parametric/data_file.m
+++ dat_parametric/data_file.m
@@ -1,19 +1,26 @@
 clc; clear;
 
-%% test
-
-% examplary file
-filename = 'example_model_0';
-new_property_number = 264;
-
-new_file = change_material_properties(filename,new_property_number);
-
-%% Linux bash commands
+%% setup of example data files
 
 % 3 thermal loadcases
-loadcases = ['example_model_0', 'example_model_0', 'example_model_0'];
-
+loadcases = ["composite_cond_T_xx_0_5", "composite_cond_T_yy_0_5", "composite_cond_T_zz_0_5"];
 new_property_number = 364;
+
+% check if files exist in the current folder
+file_counter = 0;
+for i = 1:length(loadcases)
+    if isfile(strcat(loadcases(i),".dat"))
+        file_counter = file_counter + 1;
+        
+        if file_counter == length(loadcases)
+            disp('All .dat files exist')
+        end
+    else
+       disp(strcat(strcat(loadcases(i),".dat"), " does not exist")); 
+    end
+end
+
+%%
 
 for i = 1:length(loadcases)
     filename = loadcases(i);
